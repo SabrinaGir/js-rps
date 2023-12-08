93977c0f14dc93bc486d403b8943e1bbc2d5f94a
SabrinaGir <116656240+SabrinaGir@users.noreply.github.com>
2023-12-08 12:57:05 -0500

    Code doesn't work, tried fixing it

--- script.js
+++ script.js
@@ -6,20 +6,38 @@
     const buttons = document.querySelectorAll('button')
 
     let computerSelection = getComputerChoice(choices);
-    let playerSelection = getPlayerChoice();
+    let playerChoice;
+ 
+    runGame();
 
-    console.log(`Computer: ${computerSelection}`);
-    console.log("Player: " + playerSelection);
-    
-    // gets players choice from button press
-    function getPlayerChoice() {
-       
-        let playerChoice;
+    // run game
+    function runGame() {
+        
         buttons.forEach(button => button.addEventListener('click', () => {
+            
+            let computerSelection = getComputerChoice(choices);
+            let playerChoice;
+            
             playerChoice = button.textContent;
+            console.log("Computer: " + computerSelection);
+            console.log("Player: " + playerChoice);
+
+            let winner = playRound(computerSelection, playerChoice);
+
+            console.log("Winner: " + winner); // 1 = player | 2 = computer
+
+            if (winner == 'player') {
+                playerPoints = playerPoints + 1;
+                console.log("Player Score: " + playerPoints);
+
+            }
+            else if (winner == 'computer') {
+                computerPoints = computerPoints + 1;
+                console.log("Computer Score: " + computerPoints);
+            }
+            
+
         }))
-    
-        return playerChoice;
     
     }
 
@@ -33,65 +51,38 @@
     }
 
     // figure out who wins a round
-    function playRound(computerSelection, playerSelection) {
-
+    function playRound(computerSelection, playerChoice) {
+    
         // player wins
-        if (playerSelection == "rock" && computerSelection == "scissors") {
+        if (playerChoice == "rock" && computerSelection == "scissors") {
             console.log("You win! Rock beats scissors");
             return 'player';
         }
-        if (playerSelection == "paper" && computerSelection == "rock") {
+        if (playerChoice == "paper" && computerSelection == "rock") {
             console.log("You win! Paper beats rock");
             return 'player';
         }
-        if (playerSelection == "scissors" && computerSelection == "paper") {
+        if (playerChoice == "scissors" && computerSelection == "paper") {
             console.log("You win! Scissors beats paper");
             return 'player';
         }
 
         // computer wins
-        if (computerSelection == "rock" && playerSelection == "scissors") {
+        if (computerSelection == "rock" && playerChoice == "scissors") {
             console.log("You lose! Rock beats scissors");
             return 'computer';
         }
-        if (computerSelection == "paper" && playerSelection == "rock") {
+        if (computerSelection == "paper" && playerChoice == "rock") {
             console.log("You lose! Paper beats rock");
             return 'computer';
         }
-        if (computerSelection == "scissors" && playerSelection == "paper") {
+        if (computerSelection == "scissors" && playerChoice == "paper") {
             console.log("You lose! Scissors beats paper");
             return 'computer';
         }
 
-        if (playerSelection == computerSelection) {
+        if (playerChoice == computerSelection) {
             console.log("It's a tie!");
             return 'tie';
         }
     }
-
-
-
-    // // run the game  
-    // function game() {
-        
-    //     let computerSelection = getComputerChoice(choices);
-    //     let playerSelection = getPlayerChoice();
-
-    //     console.log("Computer: " + computerSelection);
-    //     console.log("Player: " + playerSelection);
-
-    //     let winner = playRound(computerSelection, playerSelection);
-
-    //     console.log("Winner: " + winner); // 1 = player | 2 = computer
-
-    //     if (winner == 'player') {
-    //         playerPoints = playerPoints + 1;
-    //         console.log("Player Score: " + playerPoints);
-
-    //     }
-    //     else if (winner == 'computer') {
-    //         computerPoints = computerPoints + 1;
-    //         console.log("Computer Score: " + computerPoints);
-    //     }
-    // }
-    
